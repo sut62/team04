84ae457bdc836262caaa2b09331aabf75900a768
ghostonlineq <layfon2002@gmail.com>
2019-12-07 15:33:59 +0700

    close #42 - Add Backend Folder

---
+++ Backend/src/main/java/com/SE/RoomBook/Entity/Gender.java
@@ -0,0 +1,32 @@
+package com.SE.RoomBook.Entity;
+
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.FetchType;
+
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name="GENDER")
+public class Gender {
+	@Id 
+	@SequenceGenerator(name="gender_seq",sequenceName="gender_seq")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="gender_seq")
+	@Column(name="Gender_ID",unique = true, nullable = true)
+	private @NonNull Long id;
+	private @NonNull String gender;
+	
+	
+}
