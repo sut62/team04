aa4741490bff75f75b9935ea1ae3582fa53cb1c2
b6002688 <mookneo1@gmail.com>
2019-12-11 01:27:20 +0700

    close #55 - ทำ entity Status สำหรับเก็บข้อมูลสถานะของห้อง

--- Backend/src/main/java/com/SE/RoomBook/Entity/Status.java
+++ Backend/src/main/java/com/SE/RoomBook/Entity/Status.java
@@ -3,11 +3,8 @@ package com.SE.RoomBook.Entity;
 import lombok.*;
 
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.FetchType;
 
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -15,17 +12,25 @@ import java.util.Collection;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Data
 @Entity
 @NoArgsConstructor
 @Table(name="STATUS")
 public class Status {
-	@Id 
-	@SequenceGenerator(name="Status_seq",sequenceName="Status_seq")
-    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="Status_seq")
-	@Column(name="Status_ID",unique = true, nullable = true)
-	private @NonNull Long id;
-	private @NonNull String Status;
-	
+    @Id
+    @SequenceGenerator(name="Status_SEQ",sequenceName="Status_SEQ")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="Status_SEQ")
+    @Column(name="Status_id",unique = true, nullable = true)
+    private @NonNull Long Status_id;
+    @Column(name="Status_name")
+    private @NonNull String Status_name;
+
+    //@OneToMany(fetch = FetchType.EAGER)
+    //mappedBy  = "Status"
+    //private Collection<ManageStatus> ManageStatus;
 }
+
