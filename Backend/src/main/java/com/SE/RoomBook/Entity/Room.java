4da5dc30c71b3166e5286954d0062379d867b19b
b6002688 <mookneo1@gmail.com>
2019-12-10 23:48:58 +0700

    close #50 - ทำ entity Room สำหรับเก็บข้อมูลของห้อง

---
+++ Backend/src/main/java/com/SE/RoomBook/Entity/Room.java
@@ -0,0 +1,35 @@
+package com.okta.springbootvue.entity;
+
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import java.util.Collection;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name="ROOMS")
+public class Room {
+    @Id
+    @SequenceGenerator(name="room_seq",sequenceName="room_seq")               
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="room_seq")  
+    @Column(name = "room_id", unique = true, nullable = true)
+    private @NonNull Long id;
+    @Column(name = "room_name")
+    private @NonNull String name;
+
+
+    @OneToMany(fetch = FetchType.EAGER)
+    // mappedBy  = "Room"
+    private Collection<ManageStatus> ManageStatus;
+}
