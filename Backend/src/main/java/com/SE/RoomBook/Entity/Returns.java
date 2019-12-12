4f5dd4115b904adf55dd95b6e6fa323711200ec2
Waratip Nammaee <wara2541.14@gmail.com>
2019-12-12 15:30:08 +0700

    แก้ code Entity returns

--- Backend/src/main/java/com/SE/RoomBook/Entity/Returns.java
+++ Backend/src/main/java/com/SE/RoomBook/Entity/Returns.java
@@ -1,13 +1,21 @@
 package com.okta.springbootvue.entity;
 
 import lombok.*;
-
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import java.util.Collection;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
