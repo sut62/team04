845a439c91b607e314fd35dbfa87c5442e1d77bb
B6009793 <Waritsaramin7921@gmail.com>
2019-12-12 22:49:48 +0700

    close #60 - ทำการเพิ่ม Entity EquipmentType

---
+++ Backend/src/main/java/com/SE/RoomBook/Entity/EquipmentType.java
@@ -0,0 +1,33 @@
+package com.SE.RoomBook.Entity;
+
+import lombok.*;
+
+import javax.persistence.Id;
+
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "EquipmentType")
+public class EquipmentType {
+    @Id
+    @SequenceGenerator(name = "Equipmenttype_SEQ", sequenceName = "equipmenttype_SEQ")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "equipmenttype_SEQ")
+    
+    private @NonNull Long equipmenttype_id;
+    private @NonNull String type;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private Collection<EquipmentType> EquipmentType;
+}
