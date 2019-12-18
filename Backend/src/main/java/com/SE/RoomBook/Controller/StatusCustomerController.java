5745a2e6917fa4ccf1ed8f32b33b21908f851552
ghostonlineq <layfon2002@gmail.com>
2019-12-18 21:12:52 +0700

    close #79 - add Gender,StatusCustomer and TitleName Controller

---
+++ Backend/src/main/java/com/SE/RoomBook/Controller/StatusCustomerController.java
@@ -0,0 +1,32 @@
+package com.SE.RoomBook.Controller;
+
+import com.SE.RoomBook.Entity.StatusCustomer;
+import com.SE.RoomBook.Repository.StatusCustomerRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+public class StatusCustomerController {
+
+    @Autowired
+    private final StatusCustomerRepository statusCustomerRepository;
+
+    public StatusCustomerController(StatusCustomerRepository statusCustomerRepository) {
+        this.statusCustomerRepository = statusCustomerRepository;
+    }
+
+    @GetMapping("/statusCustomer")
+    public Collection<StatusCustomer> StatusCustomers() {
+        return statusCustomerRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+}
---
+++ Backend/src/main/java/com/SE/RoomBook/Controller/TitleNameController.java
@@ -0,0 +1,32 @@
+package com.SE.RoomBook.Controller;
+
+import com.SE.RoomBook.Entity.TitleName;
+import com.SE.RoomBook.Repository.TitleNameRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+public class TitleNameController {
+
+    @Autowired
+    private final TitleNameRepository titlenameRepository;
+
+    public TitleNameController(TitleNameRepository titlenameRepository) {
+        this.titlenameRepository = titlenameRepository;
+    }
+
+    @GetMapping("/title")
+    public Collection<TitleName> TitleNames() {
+        return titlenameRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+}
