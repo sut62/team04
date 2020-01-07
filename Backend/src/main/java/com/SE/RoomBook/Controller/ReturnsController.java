746405d86fadd2ad448f2fec1b448ecc4ca902a3
waratip <wara2541.14@gmail.com>
2020-01-07 22:14:13 +0700

    close #23 - ทำ ReturnsController ระบบคืนอุปกรณ์

---
+++ Backend/src/main/java/com/SE/RoomBook/Controller/ReturnsController.java
@@ -0,0 +1,63 @@
+package com.SE.RoomBook.Controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonParseException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.net.URLDecoder;
+import com.SE.RoomBook.Entity.*;
+import com.SE.RoomBook.Repository.*;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+public class ReturnsController {
+    @Autowired
+    private final ReturnsRepository returnsRepository;
+    @Autowired
+    private BorrowRepository borrowRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    ReturnsController(ReturnsRepository returnsRepository) {
+        this.returnsRepository = returnsRepository;
+    }
+
+    @GetMapping("/returns")
+    public Collection<Returns> Returns() {
+        return returnsRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+    @PostMapping("/returns/{M}/{Br}/{E}")
+    public Returns newReturns(Returns newReturns, @PathVariable long C, @PathVariable long Br, @PathVariable long E) {
+
+        Employee employee = employeeRepository.findById(E);
+        Borrow borrow = borrowRepository.findById(Br);
+        Customer customer = customerRepository.findById(C);
+
+        newReturns.setBorrow(borrow);
+        newReturns.setCustomer(customer);
+        newReturns.setEmployee(employee);
+        newReturns.setReturnsdate(new Date());
+
+        return returnsRepository.save(newReturns);
+
+    }
+}
