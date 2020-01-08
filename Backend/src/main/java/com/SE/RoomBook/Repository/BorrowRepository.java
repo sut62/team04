0df3e6e594394c12c326e55c4f0e0df0c17181c6
JktJakkrit <57915214+JktJakkrit@users.noreply.github.com>
2020-01-08 22:45:56 +0700

    close #107 - แก้ code ใน BorrowRepository.java

--- Backend/src/main/java/com/SE/RoomBook/Repository/BorrowRepository.java
+++ Backend/src/main/java/com/SE/RoomBook/Repository/BorrowRepository.java
@@ -1,12 +1,15 @@
 package com.SE.RoomBook.Repository;
 
-import com.SE.RoomBook.Entity.Borrow;
+import java.util.Collection;
 
-
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.SE.RoomBook.Entity.*;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.repository.query.Param;
 
 @RepositoryRestResource
 public
 interface BorrowRepository extends JpaRepository<Borrow, Long> {
+    Borrow findById(long id);
+    
 }
