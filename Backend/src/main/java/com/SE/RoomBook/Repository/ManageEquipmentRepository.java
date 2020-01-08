2871c47bbacd2167a3ccd10e4dc8acf262a2bc03
B6009793 <waritsaramin7921@gmail.com>
2020-01-08 11:47:05 +0700

    close #105 - ทำการแก้ไข ManageEquipmentRepository

--- Backend/src/main/java/com/SE/RoomBook/Repository/ManageEquipmentRepository.java
+++ Backend/src/main/java/com/SE/RoomBook/Repository/ManageEquipmentRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface ManageEquipmentRepository extends JpaRepository<ManageEquipment, Long> {
 	ManageEquipment findById(long manageEquipment_id);
+
 }
