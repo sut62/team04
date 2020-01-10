7f7b1db9b0c0fc5114d2bd71702222d773df4e18
ghostonlineq <layfon2002@gmail.com>
2020-01-10 13:45:52 +0700

    close #126 - fix Dashbord and fix router

--- Frontend/src/router.js
+++ Frontend/src/router.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Register from './components/Register'
 import Login from './components/Login'
-
+import Dashboad from './components/Dashboad'
+import Borrow from './components/Borrow'
+import ManageEquipment from './components/ManageEquipment'
+import ManageStatus from './components/ManageStatus'
+import Returns from './components/Returns'
 
 Vue.use(Router)
 
@@ -19,6 +23,31 @@ export default new Router({
       path: '/',
       name:'Login',
       component: Login
+    },
+    {
+      path: '/Dashboad',
+      name:'Dashboad',
+      component: Dashboad
+    },
+    {
+      path: '/Borrow',
+      name:'Borrow',
+      component: Borrow
+    },
+    {
+      path: '/ManageEquipment',
+      name:'ManageEquipment',
+      component: ManageEquipment
+    },
+    {
+      path: '/ManageStatus',
+      name:'ManageStatus',
+      component: ManageStatus
+    },
+    {
+      path: '/Returns',
+      name:'Returns',
+      component: Returns
     },
   
   ]
