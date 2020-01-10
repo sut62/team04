97a34b91ba4e964883094ff8e945e90ee18361de
Ittikorn Chawkamud <57912233+aseaways1234@users.noreply.github.com>
2020-01-10 21:05:51 +0700

    close #145 -เพิ่มข้อูลหลัก

--- Backend/src/main/java/com/SE/RoomBook/RoomBookApplication.java
+++ Backend/src/main/java/com/SE/RoomBook/RoomBookApplication.java
@@ -16,28 +16,23 @@ public class RoomBookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RoomBookApplication.class, args);
 	}
+
 	@Bean
-	ApplicationRunner init(GenderRepository genderRepository, 
-	TitleNameRepository titleNameRepository, 
-	StatusCustomerRepository statusCustomer,
-	EmployeeRepository employeeRepository,
-	PurposeRoomRepository purposeRoomRepository,
-	RoomRepository roomRepository, 
-	StatusRepository statusRepository)
-	{
+	ApplicationRunner init(GenderRepository genderRepository, TitleNameRepository titleNameRepository,
+			StatusCustomerRepository statusCustomer, EmployeeRepository employeeRepository,
+			PurposeRoomRepository purposeRoomRepository, RoomRepository roomRepository,
+			StatusRepository statusRepository) {
 		return args -> {
-		Employee em1 = new Employee();
+			Employee em1 = new Employee();
 			em1.setEm_name("Dodo");
 			em1.setPassword("1234");
 			employeeRepository.save(em1);
 
-			
 			Employee em2 = new Employee();
 			em2.setEm_name("Mook");
 			em2.setPassword("5678");
 			employeeRepository.save(em2);
 
-			
 			Employee em3 = new Employee();
 			em3.setEm_name("Prow");
 			em3.setPassword("9876");
@@ -49,7 +44,7 @@ public class RoomBookApplication {
 			employeeRepository.save(em4);
 
 			Stream.of("Man", "Female").forEach(Gender -> {
-                Gender gender = new Gender(); // สร้าง Object Gender
+				Gender gender = new Gender(); // สร้าง Object Gender
 				gender.setGender(Gender); // set ชื่อ (Gender) ให้ Object ชื่อ gender
 				genderRepository.save(gender); // บันทึก Objcet ชื่อ Gender
 			});
@@ -60,7 +55,7 @@ public class RoomBookApplication {
 				titleNameRepository.save(title); // บันทึก Objcet ชื่อ Title
 			});
 
-			Stream.of("Studens", "Teacher","Normal Person").forEach(Status -> {
+			Stream.of("Studens", "Teacher", "Normal Person").forEach(Status -> {
 				StatusCustomer statuscus = new StatusCustomer(); // สร้าง Object Status
 				statuscus.setStatusCus(Status); // set ชื่อ (StatusCustomer) ให้ Object ชื่อ Status
 				statusCustomer.save(statuscus); // บันทึก Objcet ชื่อ Status
@@ -72,18 +67,30 @@ public class RoomBookApplication {
 				purposeRoomRepository.save(p); // บันทึก Objcet ชื่อ Type
 			});
 
-			
-			Stream.of("G01", "G02", "G03","S01","S02","S03","P01","P02").forEach(name -> {
-					Room room = new Room(); // สร้าง Object 
-					room.setRoom_name(name); // set ชื่อ (name) ให้ Object
-					roomRepository.save(room); // บันทึก Objcet 
+			Stream.of("G01", "G02", "G03", "S01", "S02", "S03", "P01", "P02").forEach(name -> {
+				Room room = new Room(); // สร้าง Object
+				room.setRoom_name(name); // set ชื่อ (name) ให้ Object
+				roomRepository.save(room); // บันทึก Objcet
 			});
 
 			Stream.of("ออนไลน์", "ออฟไลน์").forEach(name -> {
-					Status status = new Status(); // สร้าง Object 
-					status.setStatus_name(name); // set ชื่อ (name) ให้ Object 
-					statusRepository.save(status); // บันทึก Objcet 
-				});
+				Status status = new Status(); // สร้าง Object
+				status.setStatus_name(name); // set ชื่อ (name) ให้ Object
+				statusRepository.save(status); // บันทึก Objcet
+			});
+
+			Stream.of("Stationary", "Electronic").forEach(type -> {
+				EquipmentType EquipmentType = new EquipmentType();
+				EquipmentType.setType(type);
+				EquipmentTypeRepository.save(EquipmentType);
+			});
+
+			Stream.of("Ipad", "Writeboard pen", "Plug", "Headphones").forEach(name -> {
+				EquipmentName EquipmentName = new EquipmentName();
+				EquipmentName.setName(name);
+				EquipmentNameRepository.save(EquipmentName);
+			});
+
 		};
 	}
 
