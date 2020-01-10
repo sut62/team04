3b99227d3ec15178b92204d571b2d10385fa7d39
B6009793 <waritsaramin7921@gmail.com>
2020-01-10 08:48:40 +0700

    close #133 - ทำการแก้ไข UI ระบบจัดการอุปกรณ์

--- Frontend/src/components/ManageEquipment.vue
+++ Frontend/src/components/ManageEquipment.vue
@@ -2,37 +2,51 @@
   <div class="ManageEquipment">
     <div class="outside">
       <v-row justify="center">
-      <br>
-      <!-- <v-col justify="center"> -->
+        <br>
+        <!-- <v-col justify="center"> -->
         <!-- <v-container> -->
-          <!-- <v-layout row wrap> -->
-            <v-col cols = 3 justify="center">
+        <!-- <v-layout row wrap> -->
+        <v-col
+          cols=5
+          justify="center"
+        >
 
-              <v-toolbar color="black" dark flat>
-                <!-- กรอบข้างบนสีฟ้า -->
-                <v-toolbar-title>
+          <v-toolbar
+            color="black"
+            dark
+            flat
+          >
+            <!-- กรอบข้างบนสีฟ้า -->
+            <v-toolbar-title>
 
-                  <h2 class="headline">ระบบจัดการอุปกรณ์</h2>
+              <h2 class="headline ">
 
-                </v-toolbar-title>
-                <div class="flex-grow-1"></div>
-              </v-toolbar>
-              <br>
-              <br>
+                ระบบจัดการอุปกรณ์
 
-              <v-select
+              </h2>
+
+            </v-toolbar-title>
+            <div class="flex-grow-1"></div>
+          </v-toolbar>
+          <br>
+          <br>
+          <v-sheet>
+
+            <v-select
+              v-if="lock"
               v-model="ManageEquipment.EmName"
               :items="Employee"
               label="Employee"
               item-text="em_name"
               item-value="em_id"
+              :readonly="true"
               solo
               outlined
               dense
-              ></v-select>
+            ></v-select>
 
-              <br>
-              <v-select
+            <br>
+            <v-select
               v-model="ManageEquipment.Typename"
               :items="EquipmentType"
               label="EquipmentType"
@@ -41,10 +55,10 @@
               solo
               outlined
               dense
-              ></v-select>
+            ></v-select>
 
-              <br>
-              <v-select
+            <br>
+            <v-select
               v-model="ManageEquipment.EQname"
               :items="EquipmentName"
               label="EquipmentName"
@@ -53,23 +67,28 @@
               solo
               outlined
               dense
-              ></v-select>
+            ></v-select>
 
-              <br>
-              <v-text-field
+            <br>
+            <v-text-field
               v-model="ManageEquipment.Amount"
               label="amount"
               :rules="[(v) => !!v || 'Item is required']"
               required
               solo
-              ></v-text-field>
+              outlined
+            ></v-text-field>
 
-              <br>
-              <v-btn small @click="saveData">Add Equipment</v-btn>
-              <!-- @click="saveData" -->
-            </v-col>
+            <br>
+            <v-btn
+              small
+              @click="saveData"
+            >Add Equipment</v-btn>
+          </v-sheet>
+          <!-- @click="saveData" -->
+        </v-col>
 
-            <!-- <v-col cols = 7>
+        <!-- <v-col cols = 7>
               <br>
               <br>
               <br>
@@ -102,92 +121,102 @@
             </v-card>
           </v-col> -->
         <!-- </v-layout> -->
-      <!-- </v-container> -->
-    <!-- </v-col> -->
-  </v-row>
+        <!-- </v-container> -->
+        <!-- </v-col> -->
+      </v-row>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import http from "../https";
+/* eslint-disable no-console */
+import http from "../http-common";
 export default {
-  name:"ManageEquipment",
-  data(){
-    return{
-      ManageEquipment:{
+  name: "ManageEquipment",
+  data() {
+    return {
+      ManageEquipment: {
         EmName: "",
         Typename: "",
         EQname: "",
-        Amount: "",
+        Amount: ""
       },
 
-      Employee:[],
-
-      EquipmentName:[],
-      EquipmentType:[],
-      EquipmentAmount:"",
-
-      showtable:[]
+      Employee: [],
+      EquipmentName: [],
+      EquipmentType: [],
+      EquipmentAmount: "",
+      showtable: [],
+      lock: "",
+      employee: ""
     };
   },
-  methods:{
-    saveData(){
-      http.post("/manageEquipments/"+
-      this.ManageEquipment.EmName
-      +"/"+
-      this.ManageEquipment.Typename
-      +"/"+
-      this.ManageEquipment.EQname
-      +"/" +
-      this.ManageEquipment.Amount)
-      .then(response => {
-        console.log(response);
-        alert("เพิ่มเมนูสำเร็จ")
-        location.reload();
-        // this.getDataTable();
-
-      })
-      .catch(e => {
-        console.log(e);
-        alert("เพิ่มเมนูไม่สำเร็จ")
-      })
-      ;
+  methods: {
+    lockemployee() {
+      this.emid = this.$route.params.em;
+      this.employee = this.emid;
+      this.ManageEquipment.EmName = this.employee;
+      this.lock = true;
+    },
+    saveData() {
+      http
+        .post(
+          "/manageEquipments/" +
+            this.ManageEquipment.EmName +
+            "/" +
+            this.ManageEquipment.Typename +
+            "/" +
+            this.ManageEquipment.EQname +
+            "/" +
+            this.ManageEquipment.Amount
+        )
+        .then(response => {
+          console.log(response);
+          alert("เพิ่มเมนูสำเร็จ");
+          // location.reload();
+          // this.getDataTable();
+          this.ManageEquipment.Typename = "";
+          this.ManageEquipment.EQname = "";
+          this.ManageEquipment.Amount = "";
+        })
+        .catch(e => {
+          console.log(e);
+          alert("เพิ่มเมนูไม่สำเร็จ");
+        });
       this.submitted = true;
     },
-    getEmployee(){
-
+    getEmployee() {
       http.get("/employees").then(response => {
         this.Employee = response.data;
       });
       this.submitted = true;
     },
-    getEquipmentType(){
-
-      http.get("/equipmentTypes").then(response =>{
+    getEquipmentType() {
+      http.get("/equipmentTypes").then(response => {
         this.EquipmentType = response.data;
       });
       this.submitted = true;
     },
-    getEquipmentName(){
-      http.get("/equipmentNames").then(response =>{
+    getEquipmentName() {
+      http.get("/equipmentNames").then(response => {
         this.EquipmentName = response.data;
       });
       this.submitted = true;
-
     },
-    getDataTable(){
-      http.get("/manageEquipments").then(response =>{
+    getDataTable() {
+      http.get("/manageEquipments").then(response => {
         this.showtable = response.data;
       });
       this.submitted = true;
     }
   },
-  mounted(){
+  mounted() {
     this.getEmployee();
     this.getEquipmentType();
     this.getEquipmentName();
     this.getDataTable();
+    this.lockemployee();
   }
-}
+};
+/* eslint-disable no-console */
 </script>
