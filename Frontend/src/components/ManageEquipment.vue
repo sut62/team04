18eb6b63ce5bfc035b40f510b982790fd4cd3c8c
B6009793 <waritsaramin7921@gmail.com>
2020-01-21 07:28:50 +0700

    close #185 - แก้ไข UI ระบบจัดการอุปกรณ์

--- Frontend/src/components/ManageEquipment.vue
+++ Frontend/src/components/ManageEquipment.vue
@@ -7,125 +7,146 @@
         <!-- <v-container> -->
         <!-- <v-layout row wrap> -->
         <v-col
-          cols=5
-          justify="center"
+        cols=5
+        justify="center"
         >
 
-          <v-toolbar
-            color="black"
-            dark
-            flat
-          >
-            <!-- กรอบข้างบนสีฟ้า -->
-            <v-toolbar-title>
+        <v-toolbar
+        color="black"
+        dark
+        flat
+        >
+        <!-- กรอบข้างบนสีฟ้า -->
+        <v-toolbar-title>
 
-              <h2 class="headline ">
+          <h2 class="headline ">
 
-                ระบบจัดการอุปกรณ์
+            ระบบจัดการอุปกรณ์
 
-              </h2>
+          </h2>
 
-            </v-toolbar-title>
-            <div class="flex-grow-1"></div>
-          </v-toolbar>
-          <br>
-          <br>
-          <v-sheet>
+        </v-toolbar-title>
+        <div class="flex-grow-1"></div>
+      </v-toolbar>
+      <br>
+      <br>
+      <v-sheet>
 
-            <v-select
-              v-if="lock"
-              v-model="ManageEquipment.EmName"
-              :items="Employee"
-              label="Employee"
-              item-text="em_name"
-              item-value="em_id"
-              :readonly="true"
-              solo
-              outlined
-              dense
-            ></v-select>
+        <v-select
+        v-if="lock"
+        v-model="ManageEquipment.EmName"
+        :items="Employee"
+        label="Employee"
+        item-text="em_name"
+        item-value="em_id"
+        :readonly="true"
+        solo
+        outlined
+        dense
+        ></v-select>
 
-            <br>
-            <v-select
-              v-model="ManageEquipment.Typename"
-              :items="EquipmentType"
-              label="EquipmentType"
-              item-text="type"
-              item-value="equipmenttype_id"
-              solo
-              outlined
-              dense
-            ></v-select>
+        <br>
+        <v-select
+        v-model="ManageEquipment.Typename"
+        :items="EquipmentType"
+        label="EquipmentType"
+        item-text="type"
+        item-value="equipmenttype_id"
+        solo
+        outlined
+        dense
+        ></v-select>
 
-            <br>
-            <v-select
-              v-model="ManageEquipment.EQname"
-              :items="EquipmentName"
-              label="EquipmentName"
-              item-text="name"
-              item-value="equipmentname_id"
-              solo
-              outlined
-              dense
-            ></v-select>
+        <br>
+        <v-select
+        v-model="ManageEquipment.EQname"
+        :items="EquipmentName"
+        label="EquipmentName"
+        item-text="name"
+        item-value="equipmentname_id"
+        solo
+        outlined
+        dense
+        ></v-select>
 
-            <br>
-            <v-text-field
-              v-model="ManageEquipment.Amount"
-              label="amount"
-              :rules="[(v) => !!v || 'Item is required']"
-              required
-              solo
-              outlined
-            ></v-text-field>
+        <br>
+        <v-text-field
+        v-model="ManageEquipment.Amount"
+        label="amount"
+        :rules="[(v) => !!v || 'Item is required']"
+        required
+        solo
+        outlined
+        ></v-text-field>
 
-            <br>
-            <v-btn
-              small
-              @click="saveData"
-            >Add Equipment</v-btn>
-          </v-sheet>
-          <!-- @click="saveData" -->
-        </v-col>
+        <br>
+        <!-- <v-btn
+        small
+        @click="saveData"
+        >Add Equipment</v-btn> -->
+        <div class="text-center">
+        <v-btn
+        @click="saveData"
+        tile
+        color="success">Add equipment</v-btn>
+      </div>
 
-        <!-- <v-col cols = 7>
-              <br>
-              <br>
-              <br>
-              <br>
-              <v-card
-              class="mx-auto"
-              max-width="550"
-              outlined
-              >
-              <v-simple-table style = "center" >
-                <template v-slot:default>
-                  <thead>
-                    <tr>
-                      <th class="text-left">Employee</th>
-                      <th class="text-left">EquipmentType</th>
-                      <th class="text-left">EquipmentName</th>
-                      <th class="text-left">Amount</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr v-for="item in showtable" :key="item.name">
-                      <td class="text-center">{{ item.Employee.em_name }}</td>
-                      <td class="text-center">{{ item.EquipmentType.type }}</td>
-                      <td class="text-center">{{ item.EquipmentName.name }}</td>
-                      <td class="text-center">{{ item.manageEquipment_amount+" ea" }}</td>
-                    </tr>
-                  </tbody>
-                </template>
-              </v-simple-table>
-            </v-card>
-          </v-col> -->
-        <!-- </v-layout> -->
-        <!-- </v-container> -->
-        <!-- </v-col> -->
-      </v-row>
-    </div>
-  </div>
+
+        <v-snackbar
+        v-model="snackbar"
+        top
+        :color="cl"
+        >
+        {{ status }}
+        <v-btn
+        text
+        @click="snackbar = false"
+        >
+        close
+      </v-btn></v-snackbar>
+
+    </v-sheet>
+    <!-- @click="saveData" -->
+  </v-col>
+
+  <!-- <v-col cols = 7>
+  <br>
+  <br>
+  <br>
+  <br>
+  <v-card
+  class="mx-auto"
+  max-width="550"
+  outlined
+  >
+  <v-simple-table style = "center" >
+  <template v-slot:default>
+  <thead>
+  <tr>
+  <th class="text-left">Employee</th>
+  <th class="text-left">EquipmentType</th>
+  <th class="text-left">EquipmentName</th>
+  <th class="text-left">Amount</th>
+</tr>
+</thead>
+<tbody>
+<tr v-for="item in showtable" :key="item.name">
+<td class="text-center">{{ item.Employee.em_name }}</td>
+<td class="text-center">{{ item.EquipmentType.type }}</td>
+<td class="text-center">{{ item.EquipmentName.name }}</td>
+<td class="text-center">{{ item.manageEquipment_amount+" ea" }}</td>
+</tr>
+</tbody>
+</template>
+</v-simple-table>
+</v-card>
+</v-col> -->
+<!-- </v-layout> -->
+<!-- </v-container> -->
+<!-- </v-col> -->
+</v-row>
+</div>
+</div>
 </template>
 
 <script>
@@ -135,6 +156,8 @@ export default {
   name: "ManageEquipment",
   data() {
     return {
+      snackbar: false,
+      text: 'บันทึกข้อมูลสำเร็จ',
       ManageEquipment: {
         EmName: "",
         Typename: "",
@@ -160,29 +183,45 @@ export default {
     },
     saveData() {
       http
-        .post(
-          "/manageEquipments/" +
-            this.ManageEquipment.EmName +
-            "/" +
-            this.ManageEquipment.Typename +
-            "/" +
-            this.ManageEquipment.EQname +
-            "/" +
-            this.ManageEquipment.Amount
-        )
-        .then(response => {
-          console.log(response);
-          alert("เพิ่มเมนูสำเร็จ");
-          // location.reload();
-          // this.getDataTable();
-          this.ManageEquipment.Typename = "";
-          this.ManageEquipment.EQname = "";
-          this.ManageEquipment.Amount = "";
-        })
-        .catch(e => {
-          console.log(e);
-          alert("เพิ่มเมนูไม่สำเร็จ");
-        });
+      .post(
+        "/manageEquipments/" +
+        this.ManageEquipment.EmName +
+        "/" +
+        this.ManageEquipment.Typename +
+        "/" +
+        this.ManageEquipment.EQname +
+        "/" +
+        this.ManageEquipment.Amount
+      )
+      //   .then(response => {
+      //     console.log(response);
+      //     //alert("เพิ่มเมนูสำเร็จ");
+      //     // location.reload();
+      //     // this.getDataTable();
+      //     this.ManageEquipment.Typename = "";
+      //     this.ManageEquipment.EQname = "";
+      //     this.ManageEquipment.Amount = "";
+      //   })
+      //   .catch(e => {
+      //     console.log(e);
+      //     //alert("เพิ่มเมนูไม่สำเร็จ");
+      //   });
+      // this.submitted = true;
+
+      .then(response => {
+        console.log(response);
+        this.snackbar=true
+        this.status="เพิ่มข้อมูลสำเร็จ!"
+        this.cl="success"
+        //this.getDataTable();
+      })
+      .catch(e => {
+        console.log(e);
+        this.snackbar=true
+        this.cl="error"
+        this.status="เพิ่มข้อมูลไม่สำเร็จ!"
+      })
+      ;
       this.submitted = true;
     },
     getEmployee() {
